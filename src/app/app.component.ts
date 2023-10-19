5bdcba7d50dfcaea4627bc58e8f0c8e3e33ece4b
LeandraMT <magantierleandra@gmail.com>
2023-10-19 03:08:58 +0200

    added UserRegistration and UserLogin form components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LeMoovie-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+
+  // the dialog when sign up button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '300px'
+    });
+  }
+
+  // the dialog when login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '300px'
+    });
+  }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+// Import from the API calls
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.css']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() loginData = {
+    Username: '',
+    Password: ''
+  };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void { }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
+      console.log(result);
+
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
+
+      this.dialogRef.close();
+      this.snackBar.open('Successfully logged in', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
